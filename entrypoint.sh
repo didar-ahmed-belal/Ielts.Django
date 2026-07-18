bda0ed4bc07eec8daa9f8ab2a6fcc3b64269d811
didar-ahmed-belal <228870748+didar-ahmed-belal@users.noreply.github.com>
2026-07-18 17:00:33 +0600

    vfsd

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Daphne server..."
-exec daphne -b 0.0.0.0 -p 8000 main.asgi:application
+exec daphne -b 0.0.0.0 -p 8000 core.asgi:application
