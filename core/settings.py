3335693ee3a46d095982fb02a1f20c6323369cd9
Didar Ahmed Belal <belalislam294@gmail.com>
2026-07-20 15:29:03 +0600

    Use environment variable for EMAIL_HOST
    Updated EMAIL_HOST to use environment variable.

--- core/settings.py
+++ core/settings.py
@@ -148,7 +148,7 @@ SIMPLE_JWT = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
